f9d49a0bf709a086d87a5ca092b956540ac14198
hussainbapuswellness <hussainbapuswellness@gmail.com>
2026-07-24 20:13:20 +0530

    Test language button click events

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 console.log("Language system loaded");
+  
+const btnEn = document.getElementById("btn-en");
+const btnHi = document.getElementById("btn-hi");
+const btnGu = document.getElementById("btn-gu");
 
+btnEn.addEventListener("click", () => {
+    alert("English Selected");
+});
+
+btnHi.addEventListener("click", () => {
+    alert("हिन्दी चुनी गई");
+});
+
+btnGu.addEventListener("click", () => {
+    alert("ગુજરાતી પસંદ કરવામાં આવી");
+});
 });
