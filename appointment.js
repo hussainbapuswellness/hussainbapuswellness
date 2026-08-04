60ae38358e4c8c9379ad71b267d71eb0e389f02c
hussainbapuswellness <hussainbapuswellness@gmail.com>
2026-08-04 18:22:48 +0530

    git commit -m "Connect appointment form with EmailJS"

--- appointment.js
+++ appointment.js
@@ -31,6 +31,39 @@ form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const id = generateAppointmentID();
+    const templateParams = {
+    appointment_id: id,
+    name: form.name.value,
+    email: form.email.value,
+    phone: form.phone.value,
+    country: form.country.value,
+    consultation: form.consultation.value,
+    message: form.message.value
+};
+
+emailjs.send(
+    "service_ogjealg",
+    "template_5qi5r1b",
+    templateParams
+)
+.then(function(response) {
+
+    console.log("SUCCESS!", response.status, response.text);
+
+    appointmentID.innerHTML = id;
+    form.style.display = "none";
+    successBox.style.display = "block";
+    successBox.scrollIntoView({
+        behavior: "smooth"
+    });
+
+})
+.catch(function(error) {
+
+    alert("Email sending failed.");
+    console.log(error);
+
+});
 
     appointmentID.innerHTML = id;
 
