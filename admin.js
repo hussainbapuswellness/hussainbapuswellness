458a91af8de3a8d66984afbabe53f2c638c068d1
hussainbapuswellness <hussainbapuswellness@gmail.com>
2026-08-06 19:31:45 +0530

    Admin Dashboard Part-1

--- admin.js
+++ admin.js
@@ -1,1 +1,61 @@
+async function loadPatients() {
 
+const { data, error } = await supabaseClient
+
+.from("clients")
+
+.select("*")
+
+.order("Created_At", { ascending: false });
+
+if (error) {
+
+console.log(error);
+
+return;
+
+}
+
+document.getElementById("totalPatients").innerHTML = data.length;
+
+let pending = data.filter(x => x.Appointment_Status == "Pending");
+
+document.getElementById("pendingAppointments").innerHTML = pending.length;
+
+const tbody = document.querySelector("#patientsTable tbody");
+
+tbody.innerHTML = "";
+
+data.forEach(patient => {
+
+tbody.innerHTML += `
+
+<tr>
+
+<td>${patient.Appointment_Id}</td>
+
+<td>${patient.Full_Name}</td>
+
+<td>${patient.Mobile}</td>
+
+<td>${patient.Appointment_Status}</td>
+
+<td>
+
+<button>
+
+Open
+
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+}
+
+loadPatients();
