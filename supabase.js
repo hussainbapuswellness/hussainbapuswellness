51063753b41d280bc7f2f6560ecac937709cced1
hussainbapuswellness <hussainbapuswellness@gmail.com>
2026-08-06 12:22:27 +0530

    Added Supabase Configuration and Connected Frontend

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://rvejfommdzfdkwfgsqad.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_hleB_4JlNlCjiVqnd2uQuQ_HjioxhiD";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
