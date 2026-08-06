b3eef3e3557c6e68e67a22ec479482fdab42df42
hussainbapuswellness <hussainbapuswellness@gmail.com>
2026-08-06 16:06:06 +0530

    Create Appointment System Phase-1 Foundation

--- appointment.js
+++ appointment.js
@@ -1,1 +1,61 @@
+// ======================================
+// Hussain Bapu's Wellness
+// Appointment System (Phase-1)
+// ======================================
 
+// Elements
+const form = document.getElementById("appointmentForm");
+const successBox = document.getElementById("successBox");
+const appointmentID = document.getElementById("appointmentID");
+
+// Appointment ID Generator
+function generateAppointmentID() {
+
+    const year = new Date().getFullYear();
+
+    const random = Math.floor(
+        100000 + Math.random() * 900000
+    );
+
+    return `HBW-${year}-${random}`;
+
+}
+
+// Form Submit
+form.addEventListener("submit", async function (e) {
+
+    e.preventDefault();
+
+    const appointmentId = generateAppointmentID();
+
+    const appointmentData = {
+
+        Appointment_Id: appointmentId,
+
+        Full_Name: form.name.value.trim(),
+
+        Mobile: form.mobile.value.trim(),
+
+        Whatsapp: form.whatsapp.value.trim(),
+
+        Email: form.email.value.trim(),
+
+        Age: Number(form.age.value),
+
+        Gender: form.gender.value,
+
+        Country: form.country.value.trim(),
+
+        State: form.state.value.trim(),
+
+        City: form.city.value.trim(),
+
+        Consultation_Type: form.consultationType.value,
+
+        Problem_Category: form.problemCategory.value,
+
+        Problem_Short_Description: form.problem.value.trim(),
+
+        Appointment_Status: "Pending"
+
+    };
